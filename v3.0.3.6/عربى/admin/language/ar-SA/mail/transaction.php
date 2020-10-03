a8ff1421e3edb04f7566984b7eee8961fec3d9c1
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:18:31 +0700

    Create transaction.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/mail/transaction.php
@@ -0,0 +1,9 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Text
+$_['text_subject']  = '%s - رصيد نظام العمولة';
+$_['text_received'] = 'لقد استلمت %s كرصيد في المتجر !';
+$_['text_total']    = 'رصيدك الإجمالي حالياً هو %s.';
+$_['text_credit']   = 'يمكنك اسخدام رصيدك بدءً من العملية الشرائية التالية.';
