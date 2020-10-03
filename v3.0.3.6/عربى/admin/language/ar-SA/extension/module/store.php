9fc846e580bfadea0f6a924f8da38de59c475375
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:03:16 +0700

    Create store.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/extension/module/store.php
@@ -0,0 +1,18 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']       = 'صندوق اختيار المتجر';
+
+// Text
+$_['text_extension']      = 'الموديولات';
+$_['text_success']        = 'تم التعديل !';
+$_['text_edit']           = 'تحرير';
+
+// Entry
+$_['entry_admin']         = 'لاستخدام المدراء فقط';
+$_['entry_status']        = 'الحالة';
+
+// Error
+$_['error_permission']    = 'تحذير: أنت لا تمتلك صلاحيات التعديل !';
