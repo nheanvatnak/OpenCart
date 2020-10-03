342cc08702cf81aee50ab9d49472ef3d716a939d
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:18:14 +0700

    Create extension.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/marketplace/extension.php
@@ -0,0 +1,12 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']  = 'الاضافات';
+
+// Text
+$_['text_success']   = 'تم التعديل !';
+$_['text_list']      = 'قائمة';
+$_['text_type']      = 'اختيار نوع الاضافة';
+$_['text_filter']    = 'فلتر';
