3d51d56becf17d4a34a388c30956d916018e9bae
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:17:31 +0700

    Create report.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/report/report.php
@@ -0,0 +1,12 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title'] = 'التقارير';
+
+// Text
+$_['text_success']  = 'تم التعديل !';
+$_['text_list']     = 'قائمة';
+$_['text_type']     = 'اختر نوع التقرير';
+$_['text_filter']   = 'فلتر';
