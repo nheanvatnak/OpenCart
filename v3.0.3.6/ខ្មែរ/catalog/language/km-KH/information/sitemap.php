7d926b9e6fba004404f6405052c0d4ddf8bc6101
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-04 00:06:07 +0700

    Add files via upload

---
+++ v3.0.3.6/ខ្មែរ/catalog/language/km-KH/information/sitemap.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'ផែនទីតំបន់បណ្តាញ';
+
+// Text
+$_['text_special']     = 'ការផ្តល់ជូនពិសេស';
+$_['text_account']     = 'គណនី​របស់ខ្ញុំ';
+$_['text_edit']        = 'ព​ត៌​មាន​គណនី';
+$_['text_password']    = 'Password';
+$_['text_address']     = 'សៀវភៅ​អាស័យ​ដ្ឋាន';
+$_['text_history']     = 'ប្រវត្តិការកុម្មង់';
+$_['text_download']    = 'ទាញយក';
+$_['text_cart']        = 'កន្ត្រកដើរ​ទិញ​ឥវ៉ាន់';
+$_['text_checkout']    = 'ទូទាត់';
+$_['text_search']      = 'ស្វែងរក';
+$_['text_information'] = 'ព័ត៌មាន';
+$_['text_contact']     = 'ទាក់ទង​មក​ពួក​យើង';
---
+++ v3.0.3.6/ខ្មែរ/catalog/language/km-KH/product/category.php
@@ -0,0 +1,24 @@
+<?php
+// Text
+$_['text_refine']       = 'ចម្រាញ់ការស្វែងរក';
+$_['text_product']      = 'ផលិតផល';
+$_['text_error']        = 'រកមិនឃើញប្រភេទទេ!';
+$_['text_empty']        = 'មិនមានផលិតផលត្រូវចុះបញ្ជីនៅក្នុងប្រភេទនេះទេ។';
+$_['text_quantity']     = 'បរិមាណ:';
+$_['text_manufacturer'] = 'ម៉ាក:';
+$_['text_model']        = 'កូដផលិតផល:';
+$_['text_points']       = 'ពិន្ទុរង្វាន់:';
+$_['text_price']        = 'តម្លៃ:';
+$_['text_tax']          = 'មិនរួមបញ្ចូលពន្ធ:';
+$_['text_compare']      = 'ប្រៀបធៀបផលិតផល (%s)';
+$_['text_sort']         = 'តម្រៀបតាម:';
+$_['text_default']      = 'លំនាំដើម';
+$_['text_name_asc']     = 'ឈ្មោះ (A - Z)';
+$_['text_name_desc']    = 'ឈ្មោះ (Z - A)';
+$_['text_price_asc']    = 'តម្លៃ (ទាប &gt; ខ្ពស់)';
+$_['text_price_desc']   = 'តម្លៃ (ខ្ពស់ &gt; ទាប)';
+$_['text_rating_asc']   = 'ការវាយតំលៃ (ទាបបំផុត)';
+$_['text_rating_desc']  = 'ការវាយតំលៃ (ខ្ពស់បំផុត)';
+$_['text_model_asc']    = 'ម៉ូដែល (A - Z)';
+$_['text_model_desc']   = 'ម៉ូដែល (Z - A)';
+$_['text_limit']        = 'បង្ហាញ:';
