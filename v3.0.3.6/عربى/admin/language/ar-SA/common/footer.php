14ade30fa68c635ebbaec9697ecce8553144e7ad
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 22:58:01 +0700

    Create footer.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/common/footer.php
@@ -0,0 +1,7 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Text
+$_['text_footer'] = '<a href="http://www.opencart.com" target="_blank">OpenCart</a> &copy; 2009 - ' . date('Y') . ' جميع الحقوق محفوظة<br />ترجمة <a href="http://www.opencartarab.com" target="_blank">OpenCartArab</a>';
+$_['text_version'] 	= 'الاصدار رقم %s';
