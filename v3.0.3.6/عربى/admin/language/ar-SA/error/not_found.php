fac0ceb4b84ecd70f5b3a9a80fcb90abc0d44115
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 22:59:42 +0700

    Create not_found.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/error/not_found.php
@@ -0,0 +1,9 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title'] = 'الصفحة غير موجودة!';
+
+// Text
+$_['text_not_found'] = 'الصفحة التي طلبتها غير موجودة ! الرجاء ابلاغ المدير اذا تكررت هذه المشكلة.';
