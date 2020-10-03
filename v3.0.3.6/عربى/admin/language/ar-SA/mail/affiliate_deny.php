52ac27b3fdffb9eae449564f09d5d3dd794d9470
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:18:47 +0700

    Create affiliate_deny.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/mail/affiliate_deny.php
@@ -0,0 +1,9 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Text
+$_['text_subject'] = '%s - تم رفض تفعيل حسابك في برنامج نظام العمولة !';
+$_['text_welcome'] = 'شكراً لتسجيلك في %s !';
+$_['text_denied']  = 'للأسف تم رفض طلبك. ولمزيد من الاستفسارات يمكنك التواصل مع إدارة المتجر من خلال الرابط التالي :';
+$_['text_thanks']  = 'شكراً لك,';
