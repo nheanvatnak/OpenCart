5caa17ffbfdd0aea6e1ef3fd42a422ca9d322b11
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:01:36 +0700

    Create google_sitemap.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/extension/feed/google_sitemap.php
@@ -0,0 +1,18 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']    = 'Google Sitemap';
+
+// Text
+$_['text_extension']         = 'الموديولات';
+$_['text_success']     = 'تم التعديل !';
+$_['text_edit']        = 'تحرير';
+
+// Entry
+$_['entry_status']     = 'الحالة :';
+$_['entry_data_feed']  = 'رابط الخلاصة :';
+
+// Error
+$_['error_permission'] = 'تحذير : أنت لا تمتلك صلاحيات التعديل !';
