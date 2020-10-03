ce8263277cd384a8995ebddc2eb8d9d6cb2da1f8
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:02:30 +0700

    Create html.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/extension/module/html.php
@@ -0,0 +1,21 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']       = 'موديول حر - HTML Content';
+
+// Text
+$_['text_extension']      = 'الموديولات';
+$_['text_success']        = 'تم التعديل !';
+$_['text_edit']           = 'تحرير';
+
+// Entry
+$_['entry_name']          = 'اسم الموديول';
+$_['entry_title']         = 'العنوان';
+$_['entry_description']   = 'الوصف';
+$_['entry_status']        = 'الحالة';
+
+// Error
+$_['error_permission']    = 'تحذير: أنت لا تمتلك صلاحيات التعديل!';
+$_['error_name']       = 'اسم الموديول يجب ان يكون بين 3 و 64 حرف !';
