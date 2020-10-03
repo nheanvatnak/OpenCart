dfefa07df8c37b7519ff2f90e215c27e3a8f22fb
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:02:20 +0700

    Create featured.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/extension/module/featured.php
@@ -0,0 +1,28 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']       = 'منتجات مميزة';
+
+// Text
+$_['text_extension']      = 'الموديولات';
+$_['text_success']        = 'تم التعديل!';
+$_['text_edit']           = 'تحرير';
+
+// Entry
+$_['entry_name']          = 'اسم الموديول';
+$_['entry_product']       = 'المنتجات';
+$_['entry_limit']         = 'حد أقصى';
+$_['entry_width']         = 'العرض';
+$_['entry_height']        = 'الارتفاع';
+$_['entry_status']        = 'الحالة';
+
+// Help
+$_['help_product']        = '(اكتب بداية حرف أي كلمة لتظهر القائمة المنسدلة للاستكمال التلقائي)';
+
+// Error
+$_['error_permission']    = 'تحذير: أنت لا تمتلك صلاحيات التعديل!';
+$_['error_name']       = 'اسم الموديول يجب ان يكون بين 3 و 64 حرف !';
+$_['error_width']      = 'العرض مطلوب !';
+$_['error_height']     = 'الارتفاع مطلوب !';
