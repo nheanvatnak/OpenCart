b92be3ce0118f9f2d036b9fa24074cea5d9065ab
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 22:57:50 +0700

    Create developer.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/common/developer.php
@@ -0,0 +1,28 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']    = 'إعدادات المطورين';
+
+// Text
+$_['text_success']     = 'تم تعديل الاعدادات بنجاح !';
+$_['text_theme']       = 'القالب';
+$_['text_sass']        = 'SASS';
+$_['text_cache']       = 'تم مسح الملفات المؤقتة - الكاش لـ %s !';
+
+// Column
+$_['column_component'] = 'العنصر';
+$_['column_action']    = 'تحرير';
+
+// Entry
+$_['entry_theme']      = 'القالب';
+$_['entry_sass']       = 'SASS';
+$_['entry_cache']      = 'الملفات المؤقتة - الكاش';
+
+// Button
+$_['button_on']        = 'تشغيل';
+$_['button_off']       = 'إيقاف';
+
+// Error
+$_['error_permission'] = 'تحذير : أنت لاتمتلك صلاحيات التعديل !';
