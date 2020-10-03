e84bc9f9d7d0ab57f39389d5c03ab8969d7947ce
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:16:37 +0700

    Create pickup.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/shipping/pickup.php
@@ -0,0 +1,19 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Heading
+$_['heading_title']    = 'الاستلام من المتجر';
+
+// Text
+$_['text_extension']   = 'الموديولات';
+$_['text_success']     = 'تم التعديل !';
+$_['text_edit']        = 'تحرير';
+
+// Entry
+$_['entry_geo_zone']   = 'المنطقة الجغرافية :';
+$_['entry_status']     = 'الحالة :';
+$_['entry_sort_order'] = 'ترتيب الفرز :';
+
+// Error
+$_['error_permission'] = 'تحذير : أنت لا تمتلك صلاحيات التعديل !';
