b12186f344edbce9f7b2fea8b998803b96ffd5a2
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-04 00:04:15 +0700

    Add files via upload

---
+++ v3.0.3.6/ខ្មែរ/catalog/language/km-KH/common/footer.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_information']  = 'ព័ត៌មាន';
+$_['text_service']      = 'បន្ថែម';
+$_['text_extra']        = 'បន្ថែម';
+$_['text_contact']      = 'ទាក់ទង​មក​ពួក​យើង';
+$_['text_return']       = 'ត្រឡប់';
+$_['text_sitemap']      = 'ផែនទីតំបន់បណ្តាញ';
+$_['text_manufacturer'] = 'វិញ្ញាបនបត្រអំណោយ';
+$_['text_voucher']      = 'វិញ្ញាបនបត្រអំណោយ';
+$_['text_affiliate']    = 'សម្ព័ន្ធ';
+$_['text_special']      = 'ពិសេស';
+$_['text_account']      = 'គណនី​របស់ខ្ញុំ';
+$_['text_order']        = 'ប្រវត្តិការកុម្មង់';
+$_['text_wishlist']     = 'បញ្ជីបំណង';
+$_['text_newsletter']   = 'ព្រឹត្តិប័ត្រព័ត៌មាន';
+$_['text_powered']      = 'ដំណើរការដោយ <a href="http://shop.prosperitas.asia/">x-Shop™</a><br /> %s &copy; %s';
---
+++ v3.0.3.6/ខ្មែរ/catalog/language/km-KH/common/header.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_home']          = 'គេហទំព័រ';
+$_['text_wishlist']      = 'បញ្ជីបំណង (%s)';
+$_['text_shopping_cart'] = 'កន្ត្រក​ដើរ​ទិញ​ឥវ៉ាន់';
+$_['text_category']      = 'ប្រភេទ';
+$_['text_account']       = 'គណនី​របស់ខ្ញុំ';
+$_['text_register']      = 'ចុះបញ្ជី';
+$_['text_login']         = 'Login';
+$_['text_order']         = 'ប្រវត្តិការកុម្មង់';
+$_['text_transaction']   = 'ស្វែងរក';
+$_['text_download']      = 'ស្វែងរក';
+$_['text_logout']        = 'Logout';
+$_['text_checkout']      = 'ទូទាត់';
+$_['text_search']        = 'ស្វែងរក';
+$_['text_all']           = 'បង្ហាញ​ទាំងអស់';
