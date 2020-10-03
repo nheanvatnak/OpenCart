25995b57716944fb570b7872506d69d60ded85cd
nheanvatnak <52122340+nheanvatnak@users.noreply.github.com>
2020-10-03 23:18:36 +0700

    Create reward.php

---
+++ v3.0.3.6/عربى/admin/language/ar-SA/mail/reward.php
@@ -0,0 +1,8 @@
+<?php
+//  Website: WWW.OpenCartArab.com
+//  E-Mail : info@OpenCartArab.com
+
+// Text
+$_['text_subject']  = '%s - نقاط المكافآت';
+$_['text_received'] = 'لقد استلمت %s نقاط مكافآت !';
+$_['text_total']    = 'إجمالي نقاط المكافآت الخاصة حالياً هو %s.';
